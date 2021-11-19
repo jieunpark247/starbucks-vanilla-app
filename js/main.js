f3072b4b073920750e63fdb2e17f1e0caeda3765
jieunpark247 <jieunpark247@gmail.com>
2021-11-19 17:14:41 +0900

    전역 버튼 스타일

--- js/main.js
+++ js/main.js
@@ -13,4 +13,26 @@ searchInputEl.addEventListener('focus', function () {
 searchInputEl.addEventListener('blur', function () {
     searchEl.classList.remove('focused');
     searchInputEl.setAttribute('placeholder', '');
-})
+});
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function(){
+    console.log(window.scrollY);
+    if(window.scrollY > 500) {
+        //배지 숨기기
+        // badgeEl.style.display = 'none';
+       // gsap.to(요소, 지속시간, 옵션)
+       gsap.to(badgeEl, .6 , {
+          opacity: 0,
+          display: 'none'
+       });
+    }else {
+        //배지 보이기 
+        //badgeEl.style.display = 'block';
+        gsap.to(badgeEl, .6 , {
+            opacity: 100,
+            display: 'block'
+         });
+    }
+}, 300));
