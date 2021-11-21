ed1d80e95c2fde76d46ec21720935af1c7129af8
jieunpark247 <jieunpark247@gmail.com>
2021-11-21 22:43:27 +0900

    20. 슬라이더 내용 추가

--- js/main.js
+++ js/main.js
@@ -31,8 +31,44 @@ window.addEventListener('scroll', _.throttle(function(){
         //배지 보이기 
         //badgeEl.style.display = 'block';
         gsap.to(badgeEl, .6 , {
-            opacity: 100,
+            opacity: 1,
             display: 'block'
          });
     }
 }, 300));
+
+// section visual 이미지 하나하나 나오게 하기
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function(fadeEl, index){
+    gsap.to(fadeEl, 1, {
+        delay: (index + 1) * .7, //0.7초에 한번씩 실행 0.7, 1.4 , 2.1 ....
+        opacity: 1
+    });
+})
+
+//swiper
+// new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper-container', {
+    direction: 'vertical', // 수직 슬라이드
+    autoplay: true, // 자동 재생 여부
+    loop: true // 반복 재생 여부
+  })
+ 
+new Swiper('.promotion .swiper-container', {
+    // direction: 'horizontal', // 수평 슬라이드 //기본값이라 따로 명시할 필요 없음 
+    autoplay: { // 자동 재생 여부
+      delay: 5000 // 5초마다 슬라이드 바뀜
+    },
+    loop: true, // 반복 재생 여부
+    slidesPerView: 3, // 한 번에 보여줄 슬라이드 개수
+    spaceBetween: 10, // 슬라이드 사이 여백
+    centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+    pagination: { // 페이지 번호 사용 여부
+      el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+      clickable: true // 사용자의 페이지 번호 요소 제어 가능 여부
+    },
+    navigation: { // 슬라이드 이전/다음 버튼 사용 여부
+      prevEl: '.promotion .swiper-prev', // 이전 버튼 선택자
+      nextEl: '.promotion .swiper-next' // 다음 버튼 선택자
+    }
+  })
